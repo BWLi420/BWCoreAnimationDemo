1a299bd5feee81598f058418c2a78b025c9c1991
mortal-master <mortal_lbw@outlook.com>
2017-12-25 10:28:24 +0800

    CATransformLayer

--- BWCoreAnimationDemo/BWCoreAnimationDemo/Main/SpecialViewController.m
+++ BWCoreAnimationDemo/BWCoreAnimationDemo/Main/SpecialViewController.m
@@ -20,7 +20,9 @@
     
 //    [self shapeLayer];
     
-    [self textLayer];
+//    [self textLayer];
+    
+    [self transformLayer];
 }
 
 #pragma mark - CAShapeLayer
@@ -76,6 +78,85 @@
     //choose some text
     NSString *text = @"阿哈哈哈哈哈哈哈哈啊哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈阿哈哈哈哈哈哈哈哈啊哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈";
     textLayer.string = text;
+}
+
+#pragma mark - CATransformLayer
+- (void)transformLayer {
+    
+    CATransform3D pt = CATransform3DIdentity;
+    pt.m34 = -1.0 / 500;
+    self.contentView.layer.sublayerTransform = pt;
+    
+    //set up the transform for cube 1 and add it
+    CATransform3D c1t = CATransform3DIdentity;
+    c1t = CATransform3DTranslate(c1t, -100, 0, 0);
+    c1t = CATransform3DRotate(c1t, M_PI_4, 1, 0, 0);
+    c1t = CATransform3DRotate(c1t, -M_PI_4, 0, 1, 0);
+    CALayer *cube1 = [self cubeWithTransform:c1t];
+    [self.contentView.layer addSublayer:cube1];
+    
+    //set up the transform for cube 2 and add it
+    CATransform3D c2t = CATransform3DIdentity;
+    c2t = CATransform3DTranslate(c2t, 100, 0, 0);
+    c2t = CATransform3DRotate(c2t, -M_PI_4, 1, 0, 0);
+    c2t = CATransform3DRotate(c2t, -M_PI_4, 0, 1, 0);
+    CALayer *cube2 = [self cubeWithTransform:c2t];
+    [self.contentView.layer addSublayer:cube2];
+}
+
+- (CATransformLayer *)cubeWithTransform:(CATransform3D)transform {
+    
+    CATransformLayer *cube = [CATransformLayer layer];
+    
+    //add cube face 1
+    CATransform3D ct = CATransform3DMakeTranslation(0, 0, 50);
+    [cube addSublayer:[self faceWithTransform:ct]];
+    
+    //add cube face 2
+    ct = CATransform3DMakeTranslation(50, 0, 0);
+    ct = CATransform3DRotate(ct, M_PI_2, 0, 1, 0);
+    [cube addSublayer:[self faceWithTransform:ct]];
+    
+    //add cube face 3
+    ct = CATransform3DMakeTranslation(0, -50, 0);
+    ct = CATransform3DRotate(ct, M_PI_2, 1, 0, 0);
+    [cube addSublayer:[self faceWithTransform:ct]];
+    
+    //add cube face 4
+    ct = CATransform3DMakeTranslation(0, 50, 0);
+    ct = CATransform3DRotate(ct, -M_PI_2, 1, 0, 0);
+    [cube addSublayer:[self faceWithTransform:ct]];
+    
+    //add cube face 5
+    ct = CATransform3DMakeTranslation(-50, 0, 0);
+    ct = CATransform3DRotate(ct, -M_PI_2, 0, 1, 0);
+    [cube addSublayer:[self faceWithTransform:ct]];
+    
+    //add cube face 6
+    ct = CATransform3DMakeTranslation(0, 0, -50);
+    ct = CATransform3DRotate(ct, M_PI, 0, 1, 0);
+    [cube addSublayer:[self faceWithTransform:ct]];
+    
+    cube.position = CGPointMake(self.contentView.bounds.size.width * 0.5, self.contentView.bounds.size.height * 0.5);
+    cube.transform = transform;
+    
+    return cube;
+}
+
+
+- (CALayer *)faceWithTransform:(CATransform3D)transform {
+    
+    CALayer *layer = [CALayer layer];
+    layer.frame = CGRectMake(-50, -50, 100, 100);
+    
+    CGFloat red = rand() / (double)INT_MAX;
+    CGFloat green = rand() / (double)INT_MAX;
+    CGFloat blue = rand() / (double)INT_MAX;
+    
+    layer.backgroundColor = [UIColor colorWithRed:red green:green blue:blue alpha:1.0].CGColor;
+    layer.transform = transform;
+    
+    return layer;
 }
 
 @end
